c8c7988e28ff4da54f1b596303305b212e6cd4bd
ucros <76021274+ucros@users.noreply.github.com>
2020-12-14 21:48:32 -0500

    Web en React de muestra
    Esto es para un portafolio :)

---
+++ precache-manifest.e40d92dae89655722f8fae94e5243d13.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a8074c02748ee7fe60179815b81d0e",
+    "url": "/portafolio-plantilla-react/index.html"
+  },
+  {
+    "revision": "bf979dfcbfe63947609c",
+    "url": "/portafolio-plantilla-react/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3882493224e1c81ef6d6",
+    "url": "/portafolio-plantilla-react/static/css/main.98e2b360.chunk.css"
+  },
+  {
+    "revision": "bf979dfcbfe63947609c",
+    "url": "/portafolio-plantilla-react/static/js/2.34a40f8c.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/portafolio-plantilla-react/static/js/2.34a40f8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3882493224e1c81ef6d6",
+    "url": "/portafolio-plantilla-react/static/js/main.a0542c90.chunk.js"
+  },
+  {
+    "revision": "65c789c60c0f6f851338",
+    "url": "/portafolio-plantilla-react/static/js/runtime-main.43cbfacc.js"
+  },
+  {
+    "revision": "65143a02b7c3fdf1b4f1bc9cf68b0b6c",
+    "url": "/portafolio-plantilla-react/static/media/error-404-monochrome.65143a02.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portafolio-plantilla-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
